db56953065c4b27bb6927323d1bf0d5a8340628f
wp_guy <wp_guy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-05-09 11:08:53 +0000

    Changed the Ellipsis for three points...
    git-svn-id: https://plugins.svn.wordpress.org/prettier-trackbacks/trunk@45014 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- prettier-trackbacks.php
+++ prettier-trackbacks.php
@@ -3,20 +3,20 @@
 Plugin Name: Prettier Trackbacks
 Plugin URI: http://wpguy.com/tips/customize-the-ellipsis-in-the-trackbacks/
 Description: Replaces the "[...]" for just "...". Feel free to customize it by editing the plugin in the plugin editor
-Version: 1.0
+Version: 1.0.1
 Author: Wessley Roche
 Author URI: http://wpguy.com/
 */
 
 
 // CHANGE THIS TO WHATEVER YOU WANT
-$pt_replacement = "&#8230;";
+$pt_replacement = "...";
 
 
 // Prettier Trackbacks Function
 function prettier_trackbacks($comment){
 	global $pt_replacement;
-	return str_replace("[&#8230;]", $pt_replacement, $comment);
+	return str_replace("[...]", $pt_replacement, $comment);
 }
 
 
